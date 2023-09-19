c8cb2ff5e5f4a3d4426c5f83bf32943bbc3c9d30
Julian <julianfrancor@gmail.com>
2023-09-18 23:12:36 -0500

    fix dependency issue

--- module-service/src/middlewares/error_handler.py
+++ module-service/src/middlewares/error_handler.py
@@ -1,12 +1,13 @@
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse
+from typing import Union
 
 class ErrorHandler(BaseHTTPMiddleware):
     def __init__(self, app: FastAPI) -> None:
         super().__init__(app)
 
-    async def dispatch(self, request: Request, call_next) -> Response | JSONResponse:
+    async def dispatch(self, request: Request, call_next) -> Union[Response, JSONResponse]:
         try:
             return await call_next(request)
         except Exception as e:
